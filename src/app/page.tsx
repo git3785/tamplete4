7c8b7205d9ab7c844891dab91715fdb9eb48f355
git3785 <gitcourse3@gmail.com>
2024-12-27 05:06:00 +0500

    Initial commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -36,6 +36,7 @@ export default function home (){
        <Hero2/>
        <LogoSection/>
        <LatestBlogs/>
+       <Pa
         {/* <About/> */}
         {/* <FeaturesSection/> */}
         {/* <ClientSection/> */}
